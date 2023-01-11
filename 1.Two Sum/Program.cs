85b63c71ca197a95a5da1cec593099b9127303c6
Jonibek <yarashevjonibek@gmail.com>
2023-01-11 17:17:17 +0500

    Initial commit

---
+++ 1.Two Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int number in TwoSum(new int[] { 2, 7, 11, 15, }, 9))
+            {
+                Console.WriteLine(number);
+            }
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+}
